3b2943e1f195a1aef5d86b57d1e3fea949bcb70a
Assem Ashraf <61465952+assemashraf178@users.noreply.github.com>
2022-04-20 06:04:52 +0200

    Add files via upload

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../models/section_of_ages_model.dart';
+
 Widget defaultTextFormField({
   Color backgroundColor = const Color(0xFFFFA0A0),
   required String hint,
@@ -35,5 +37,56 @@ Widget defaultTextFormField({
           suffixIconColor: const Color(0xFF204d66),
         ),
         cursorColor: const Color(0xFF262A53),
+      ),
+    );
+
+Widget sectionOfAgeListItem(
+        BuildContext context, int index, List<SectionOfAgesModel> list) =>
+    InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => list[index].nextScreen,
+          ),
+        );
+      },
+      child: Container(
+        height: 250,
+        width: 400,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+        ),
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(35.0),
+        ),
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          shadowColor: Colors.grey.shade200,
+          elevation: 17,
+          color: const Color(0XFFFFA0A0),
+          child: Column(
+            children: [
+              Image(
+                image: AssetImage(list[index].url),
+                fit: BoxFit.fill,
+                height: 180,
+                width: 400,
+              ),
+              const Spacer(),
+              Text(
+                list[index].id,
+                style: const TextStyle(
+                  color: Colors.white,
+                  decorationThickness: 2,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
       ),
     );
